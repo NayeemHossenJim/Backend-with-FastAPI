44dffffb188dab73e9a160dcb9a09fa5bdb3a440
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-11-05 16:04:40 +0600

    Get request

---
+++ main.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Basic Root Endpoint
+@app.get("/")
+async def root():
+    return {"Message": "Hell Yeah!"}
+
+# Path Parameter Example
+@app.get("/path/{name}")
+async def greet(name: str) -> dict:
+    return {"Message": f"How are you {name}?"}
+
+# Query Parameter Example
+@app.get("/query")
+async def scolding(name: str) -> dict:
+    return {"Message": f"Get the Hell out of here {name}?"}
+
+# Combining Path and Query Parameters
+@app.get("/mix/{name}")
+async def greet(name: str, age: int) -> dict:
+    return {"Message": f"How are you {name}? ", "Age": f"You are {age} years old."}
+
+# Optional Query Parameters
+@app.get("/optional/{name}")
+async def greet(name: Optional[str] = "Nayeem", age: int = 0) -> dict:
+    if name and age:
+        return {"Message": f"How are you {name}? ", "Age": f"You are {age} years old."}
+    else:
+        return {"Message": f"How are you {name}?"}
