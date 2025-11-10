4ca8e725864fa7fa6c0bd1326b21ea4378ac407a
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-11-10 15:54:04 +0600

    Complete upto project

--- project/2. book2/main.py
+++ project/2. book2/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-class Book:
-    def __init__(self, title: str, author: str, pages: int):
-        self.title = title
-        self.author = author
-        self.pages = pages
+class BookRequest(BaseModel):
+    title: str = Field(..., example="The Great Book")
+    author: str = Field(min_length=3, example="John Doe")
+    pages: int = Field(gt=0, lt=150, example=123)
+    publish_date: int = Field(gt=2020,lt=2025, example=2023)
 
 BOOKS = [
-    Book("Book One", "Author A", 300),
-    Book("Book Two", "Author B", 250),
-    Book("Book Three", "Author C", 400)
+    {"title": "Book One", "author": "Author A", "pages": 200,"publish_date":2022},
+    {"title": "Book Two", "author": "Author B", "pages": 150,"publish_date":2023},
 ]
 
 @app.get("/")
@@ -21,3 +21,14 @@ async def root():
 @app.get("/books")
 async def get_books():
     return BOOKS
+
+@app.post("/create-book")
+async def create_book(book: BookRequest):
+    BOOKS.append(book.model_dump())
+    return book
+
+@app.get("/books/publish/")
+async def get_books_by_publish_date(publish_date: int):
+    for book in BOOKS:
+        if book["publish_date"] == publish_date:
+            return book
