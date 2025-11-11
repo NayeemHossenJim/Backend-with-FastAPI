88f8de8d9b1942b720d4486f6ecec677db89a9f2
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-11-11 15:42:22 +0600

    Fetch all data by API

--- project/TODO/main.py
+++ project/TODO/main.py
@@ -1,7 +1,23 @@
 import model
-from database import engine
-from fastapi import FastAPI
+from model import Todos
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 model.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try :
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+def read_root(db: db_dependency):
+    return db.query(Todos).all()
