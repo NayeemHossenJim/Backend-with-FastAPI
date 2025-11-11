0f4998374eca1ebbe5fd3502ee0389e7b8832617
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-11-11 12:25:54 +0600

    missing column

--- project/TODO/model.py
+++ project/TODO/model.py
@@ -7,4 +7,5 @@ class Todos(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
+    priority = Column(Integer)
     completed = Column(Boolean, default=False)
