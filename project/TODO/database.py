2a05c726d6caaa8e9dc2ba4c5b5b26bc65517636
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-11-11 09:35:04 +0600

    Connection to db with env

---
+++ project/TODO/database.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
+DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+with engine.connect() as connection:
+    result = connection.execute(text("SELECT version();"))
+    print(result.fetchone())
