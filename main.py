b4e340015f6f9db5867037057badb59c043fcc65
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-11-17 17:12:52 +0600

    added jinja

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 # Essential imports
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routers import task, user, admin
+from fastapi.templating import Jinja2Templates
 from database import Base, engine
 
 # Initialize FastAPI app
 app = FastAPI()
+
+# Create database tables and templates directory
+Base.metadata.create_all(bind=engine)
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 # Health check endpoint
 @app.get("/health")
